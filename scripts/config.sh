ffe297e2af4852c90e1ac6d3d712c661b0ebebdf
amochkin <amochkin@gmail.com>
2023-05-04 15:03:07 +0200

    fix: rpcuser and password rework, deleted setup script (not used)

--- scripts/config.sh
+++ scripts/config.sh
@@ -20,10 +20,10 @@ if [ -n "${SOCKS5_PROXY+1}" ]; then
 fi
 
 ### Enable wallet:
-if printf "${WALLET_ENABLE}" | grep -q "[Yy1]"; then
+if printf "${WALLET_ENABLE}" | grep -q "[YyTt1]"; then
   export WALLET_NAME=default_wallet
   echo "Enabling wallet..."
-  if printf "${WALLET_LEGACY}" | grep -q "[Yy1]"; then
+  if printf "${WALLET_LEGACY}" | grep -q "[YyTt1]"; then
     echo "Will create legacy wallet"
     WALLET_OPTS="${WALLET_OPTS} -legacy"
   fi
@@ -32,7 +32,7 @@ if printf "${WALLET_ENABLE}" | grep -q "[Yy1]"; then
 fi
 
 ### Enable txindex:
-if printf "${TXINDEX_ENABLE}" | grep -q "[Yy1]"; then
+if printf "${TXINDEX_ENABLE}" | grep -q "[YyTt1]"; then
   echo "Enabling txindex..."
   printf "txindex=1\n" >> "${CONFIG_FILE}"
 fi
@@ -75,16 +75,17 @@ if [ -n "${NETWORK+1}" ]; then
 fi
 
 ### Setting up RPC server:
-if printf "${RPC_ENABLE}" | grep -q "[Yy1]"; then
+if printf "${RPC_ENABLE}" | grep -q "[YyTt1]"; then
   echo "Enabling RPC server..."
-  RPC_PASSWORD=${RPC_PASSWORD:-$(tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1)}
-  echo "Using password ${RPC_PASSWORD}"
-  ENCRYPTED_PASSWORD=$(./rpcpasswd.py ${RPC_PASSWORD:-})
-	RPC_AUTH="${RPC_USER:-litecoinrpc}:${ENCRYPTED_PASSWORD}"
 
 	printf "server=1\n" >> "${CONFIG_FILE}"
 	printf "rpcallowip=${RPC_ALLOW:-0.0.0.0/0}\n" >> "${CONFIG_FILE}"
-	printf "rpcauth=${RPC_AUTH}\n" >> "${CONFIG_FILE}"
+	printf "rpcuser=${RPC_USER}\n" >> "${CONFIG_FILE}"
+
+  ### Use supplied password or generate one:
+  RPC_PASSWORD=${RPC_PASSWORD:-$(tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1)}
+	printf "rpcpassword=${RPC_PASSWORD}\n" >> "${CONFIG_FILE}"
+
 	printf "\n[${NETWORK_SECTION:-main}]\nrpcport=8332\nrpcbind=${RPC_BIND:-0.0.0.0}" >> "${CONFIG_FILE}"
 fi
 
