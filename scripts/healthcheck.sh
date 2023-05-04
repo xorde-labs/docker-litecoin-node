6811af1408779ea9a319ca7f614e162afcf639f4
amochkin <amochkin@gmail.com>
2023-05-04 17:24:37 +0200

    feat: trivial healthcheck

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 ### run cli and check if it's working
+### usage (docker-compose.yml):
+# healthcheck:
+#   test: [ "CMD-SHELL", "healthcheck.sh" ]
 litecoin-cli getblockchaininfo || exit 1
